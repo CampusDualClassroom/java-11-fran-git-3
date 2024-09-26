959ae6df1edfc4a012bbe3d2d4724ff7988c92cb
Fran <franrego333@gmail.com>
2024-09-26 12:23:03 +0200

    Ejercicio 11 Completado

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -1,11 +1,50 @@
 package com.campusdual.classroom;
-
 public class Exercise11 {
     public static void main(String[] args) {
-        TVRemote redRemote = new TVRemote("Rojo");
-        TVRemote blackRemote = new TVRemote("Negro");
-        System.out.println(redRemote.getColor());
-        System.out.println(blackRemote.getColor());
-    }
+        // Crear una instancia de TVRemote
+        TVRemote TVRemote = new TVRemote("Negro");
 
+        // Encender la TV
+        TVRemote.turnOn();
+        System.out.println("Encender la TV: " + TVRemote.on);
+
+        // Subir un canal
+        TVRemote.channelUp();
+        System.out.println("Canal + -> " + TVRemote.channel);
+
+        // Bajar un canal
+        TVRemote.channelDown();
+        System.out.println("Canal - -> " + TVRemote.channel);
+
+        // Establecer el canal en 0
+        TVRemote.channel = 0;
+        System.out.println("Canal: " + TVRemote.channel);
+
+        // Intentar bajar un canal y comprobar que no se puede
+        TVRemote.channelDown();
+        System.out.println("Canal - Pero ya era 0, por lo tanto canal = " + TVRemote.channel);
+
+        // Subir el volumen
+        TVRemote.volumeUp();
+        System.out.println("Volumen + -> " + TVRemote.volume);
+
+        // Bajar el volumen
+        TVRemote.volumeDown();
+        System.out.println("Volumen - -> " + TVRemote.volume);
+
+        // Establecer el volumen en 0
+        TVRemote.volume = 0;
+        System.out.println("Volumen: " + TVRemote.volume);
+
+        // Intentar bajar el volumen y comprobar que no se puede
+        TVRemote.volumeDown();
+        System.out.println("Volumen 0. No se puede bajar más, volumen -> " + TVRemote.volume);
+
+        // Obtener el color del mando a distancia y mostrarlo por pantalla
+        System.out.println("Color de mando: " + TVRemote.color);
+
+        // Apagar la TV
+        TVRemote.turnOff();
+        System.out.println("Apagando TV: " + TVRemote.on);
+    }
 }
